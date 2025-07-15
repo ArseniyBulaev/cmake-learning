e20e7857ff7f48c39f6548175d8b4022863bcc7e
Arseny Bulaev <seni190999@mail.ru>
2025-07-15 19:24:13 +0300

    creating a library

--- cmake tutorial/main.cpp
+++ cmake tutorial/main.cpp
@@ -3,11 +3,12 @@
 #include <cmath>
 
 #include "TutorialConfig.h"
+#include "MathFunctions.h"
 
 int main(int argc, char * argv []){
     std::cout << "Project version " << basic_project_VERSION_MAJOR << "." << basic_project_VERSION_MINOR << std::endl;
     if (argc > 1){
-        std::cout << "sqrt root of " << argv[1] << " equals " << std::sqrt(std::stod(argv[1])) << std::endl;
+        mathfunctions::sqrt(std::stod(argv[1]));
     }
     else{
         std::cout << "you haven't specified namber to calculate square root" << std::endl;
