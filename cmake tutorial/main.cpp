951774669b4a3917d9280f307b8669a9ac9c9043
Arseny Bulaev <seni190999@mail.ru>
2025-07-15 18:40:40 +0300

    better code to test

--- cmake tutorial/main.cpp
+++ cmake tutorial/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 #include "TutorialConfig.h"
 
-int main(){
+int main(int argc, char * argv []){
     std::cout << "Project version " << basic_project_VERSION_MAJOR << "." << basic_project_VERSION_MINOR << std::endl;
-    std::cout << "Hello Cmake " << std::stod("99") + 1 << std::endl;
+    if (argc > 1){
+        std::cout << "sqrt root of " << argv[1] << " equals " << std::sqrt(std::stod(argv[1])) << std::endl;
+    }
+    else{
+        std::cout << "you haven't specified namber to calculate square root" << std::endl;
+    }
     return 0;
 }
