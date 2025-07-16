a55104af9b8c2b83203041730c59326f923fec12
Arseny Bulaev <seni190999@mail.ru>
2025-07-16 12:34:17 +0300

    exercise 2 - adding and option

--- cmake tutorial/MathFunctions/MathFunctions.cxx
+++ cmake tutorial/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,26 @@
 #include "MathFunctions.h"
 
 // TODO 11: include cmath
+#include <cmath>
 
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
-#include "mysqrt.h"
+#ifdef USE_MYMATH
+# include "mysqrt.h"
+#endif
 
 namespace mathfunctions {
 double sqrt(double x)
 {
+
+  double sqrt_value;
+
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
-  return detail::mysqrt(x);
+  #ifdef USE_MYMATH
+    sqrt_value = detail::mysqrt(x);
+  #else
+    sqrt_value = std::sqrt(x);
+  #endif 
+    return sqrt_value;
 }
 }
--- cmake tutorial/main.cpp
+++ cmake tutorial/main.cpp
@@ -8,7 +8,8 @@
 int main(int argc, char * argv []){
     std::cout << "Project version " << basic_project_VERSION_MAJOR << "." << basic_project_VERSION_MINOR << std::endl;
     if (argc > 1){
-        mathfunctions::sqrt(std::stod(argv[1]));
+       double sqrt_value = mathfunctions::sqrt(std::stod(argv[1]));
+       std::cout << "square root of " << argv[1] << " equals " << sqrt_value << std::endl;
     }
     else{
         std::cout << "you haven't specified namber to calculate square root" << std::endl;
