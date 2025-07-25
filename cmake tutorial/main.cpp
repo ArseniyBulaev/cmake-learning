16957c2fcb158d9978cd95deee83720313dc2f86
Arseny Bulaev <seni190999@mail.ru>
2025-07-25 21:02:36 +0300

    add comments, change project name

--- cmake tutorial/main.cpp
+++ cmake tutorial/main.cpp
@@ -6,7 +6,7 @@
 #include "MathFunctions.h"
 
 int main(int argc, char * argv []){
-    std::cout << "Project version " << basic_project_VERSION_MAJOR << "." << basic_project_VERSION_MINOR << std::endl;
+    std::cout << "Project version " << Tutorial_VERSION_MAJOR << "." <<  Tutorial_VERSION_MINOR << std::endl;
     if (argc > 1){
        double sqrt_value = mathfunctions::sqrt(std::stod(argv[1]));
        std::cout << "square root of " << argv[1] << " equals " << sqrt_value << std::endl;
